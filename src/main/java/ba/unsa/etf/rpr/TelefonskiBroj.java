9eb460e4b19f3795da709f76a13577f116be430a
Hana Catic <hcatic1@etf.unsa.ba>
2022-11-06 22:55:03 +0100

    ispravka greske u implementaciji interfejsa comparable za apstraktnu klasu telefonskiBroj

--- src/main/java/ba/unsa/etf/rpr/TelefonskiBroj.java
+++ src/main/java/ba/unsa/etf/rpr/TelefonskiBroj.java
@@ -1,11 +1,11 @@
 package ba.unsa.etf.rpr;
 
-public abstract class TelefonskiBroj { //bazna apstraktna klasa
+public abstract class TelefonskiBroj implements Comparable<TelefonskiBroj>{ //bazna apstraktna klasa
     protected String broj;
     public abstract String ispisi();
     public abstract int hashCode();
 
-    public int compareTo(TelefonskiBroj broj1, TelefonskiBroj broj2){ //implementacija metode compareTo, interfejsa Comparable
-        return broj1.ispisi().compareTo(broj2.ispisi());
+    public int compareTo(TelefonskiBroj broj1){
+        return this.ispisi().compareTo(broj1.ispisi());
     }
 }
